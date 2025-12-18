fa1277cc0aff0931fb06c9b4267e5f8d64fbdc43
leonardo1952 <lo88980@gmail.com>
2025-12-18 20:32:52 -0300

    create level1

--- lib/level1_page.dart
+++ lib/level1_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
 class Level1Page extends StatelessWidget {
   const Level1Page({super.key});
@@ -6,8 +7,27 @@ class Level1Page extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Level 1')),
-      body: Center(),
+      appBar: AppBar(title: const Text('Level 1 - Baseline')),
+      body: ListView.builder(
+        itemCount: 1000,
+        itemBuilder: (context, index) {
+          return _listTile(index);
+        },
+      ),
+    );
+  }
+
+  ListTile _listTile(int index) {
+    return ListTile(
+      leading: CachedNetworkImage(
+        imageUrl: 'https://picsum.photos/50?random=$index',
+        placeholder: (context, url) => const CircularProgressIndicator(),
+        errorWidget: (context, url, error) => const Icon(Icons.error),
+        width: 50,
+        height: 50,
+      ),
+      title: Text('Item $index'),
+      subtitle: Text('Detailed description for item $index'),
     );
   }
 }
